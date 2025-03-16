ad62de0890b5ff576175d85751c20d1afc4d711b
Indah Purnama Sari <12350121154@students.uin-suska.ac.id>
2025-03-16 11:30:17 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import QuranPage from "./pages/QuranPage";
 import Home from "./pages/Home";
-import Ayatpage from "./pages/ayatPage";
 import AsmaulHusnaPage from "./pages/AsmaulHusnaPage";
 import DoaPage from "./pages/DoaPage";
+import AyatPage from "./pages/AyatPage";
+
+
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/ayat" element={<Ayatpage/>} /> 
+        <Route path="/ayat" element={<AyatPage/>} /> 
         <Route path="/quran/:nomorSurah" element={<QuranPage />} />
         <Route path="/asmaulhusna" element={<AsmaulHusnaPage/>} />
         <Route path="/doa" element={<DoaPage/>} />
